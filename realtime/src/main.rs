34910aa3b7f529ec74307e47871dd0e6d02ecdad
the-dev-rahul <rahul.synchronizedcodelab@gmail.com>
2024-06-11 15:12:18 +0530

    Added ARC and removed static authenticatino and aggregator

--- realtime/src/main.rs
+++ realtime/src/main.rs
@@ -1,19 +1,17 @@
 use std::str::FromStr;
+use authentication::Authentication;
 use tokio_tungstenite::accept_async;
 
 use tokio::net::TcpListener;
 use futures_util::StreamExt;
-use once_cell::sync::Lazy;
 use std::sync::Mutex;
+use std::sync::Arc;
 use serde_json;
 
 mod serializers;
 mod authentication;
 mod aggregator;
 
-
-static AUTHENTICATION:Lazy<Mutex<authentication::Authentication>>  = Lazy::new(||Mutex::new(authentication::Authentication::new()));
-static AGGREGATOR:Lazy<Mutex<aggregator::Aggregator>>  = Lazy::new(||Mutex::new(aggregator::Aggregator::new()));
 
 #[tokio::main]
 async fn main() {
@@ -22,15 +20,21 @@ async fn main() {
     let listener = TcpListener::bind(addr).await.expect("Failed to bind");
     println!("Listening on: {}", addr);
 
+    let auth  = Arc::new(Mutex::new(authentication::Authentication::new()));
+    let aggrt = Arc::new(Mutex::new(aggregator::Aggregator::new()));
+
+
     while let Ok((stream, _)) = listener.accept().await {
-        tokio::spawn(  handle_connection(stream));
+        let  auth_clone =  Arc::clone(&auth); 
+        let  aggrt: Arc<Mutex<aggregator::Aggregator>> =  Arc::clone(&aggrt); 
+        tokio::spawn(  handle_connection(stream, auth_clone, aggrt));
     }
 }
 
 
-async fn handle_connection(stream: tokio::net::TcpStream) {
+async fn handle_connection(stream: tokio::net::TcpStream, auth: Arc<Mutex<Authentication>>, aggrt : Arc<Mutex<aggregator::Aggregator>>) {
 
-    async fn connect(stream: tokio::net::TcpStream) -> Result<(), Box<dyn std::error::Error>>  {
+    async fn connect(stream: tokio::net::TcpStream, auth: Arc<Mutex<Authentication>>, aggrt : Arc<Mutex<aggregator::Aggregator>>) -> Result<(), Box<dyn std::error::Error>>  {
 
         let ws_stream = accept_async(stream)
             .await?;
@@ -46,8 +50,8 @@ async fn handle_connection(stream: tokio::net::TcpStream) {
             let websocket_message: serializers::WebSocketMessage = serde_json::from_str(&message.to_string()).unwrap();
             let status = serializers::Status::from_str(&websocket_message.status).unwrap();
             
-            let mut auth = AUTHENTICATION.lock().unwrap();
-            let mut aggrt= AGGREGATOR.lock().unwrap();
+            let mut auth = auth.lock().unwrap();
+            let mut aggrt= aggrt.lock().unwrap();
             
             match status {
                 serializers::Status::Authenticate =>{
@@ -72,7 +76,7 @@ async fn handle_connection(stream: tokio::net::TcpStream) {
         Ok(())
     }
 
-    match connect(stream).await{
+    match connect(stream, auth, aggrt).await{
         Ok(_result) => {
         },
         Err(e) => {
